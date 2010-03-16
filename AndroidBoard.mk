b8fea40f63bdf3f385c5b518050e74b206125ef5
Jean-Baptiste Queru <jbq@google.com>
2010-03-16 16:44:08 -0700

    Share common AndroidBoard files between dream and sapphire
    Change-Id: Id2c6d80cbb93d53237ab4c7cb2738c8bd81c767e

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -14,27 +14,11 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifeq ($(TARGET_PREBUILT_KERNEL),)
-TARGET_PREBUILT_KERNEL := $(LOCAL_PATH)/kernel
-endif
-
-file := $(INSTALLED_KERNEL_TARGET)
-ALL_PREBUILT += $(file)
-$(file): $(TARGET_PREBUILT_KERNEL) | $(ACP)
-	$(transform-prebuilt-to-target)
-
 $(call add-radio-file,recovery/images/firmware_install.565)
-$(call add-radio-file,recovery/images/firmware_error.565)
-$(call add-radio-file,recovery/images/bitmap_size.txt)
 
 file := $(TARGET_OUT_KEYLAYOUT)/sapphire-keypad.kl
 ALL_PREBUILT += $(file)
 $(file) : $(LOCAL_PATH)/sapphire-keypad.kl | $(ACP)
-	$(transform-prebuilt-to-target)
-
-file := $(TARGET_OUT_KEYLAYOUT)/h2w_headset.kl
-ALL_PREBUILT += $(file)
-$(file) : $(LOCAL_PATH)/h2w_headset.kl | $(ACP)
 	$(transform-prebuilt-to-target)
 
 file := $(TARGET_ROOT_OUT)/init.sapphire.rc
@@ -42,23 +26,10 @@ ALL_PREBUILT += $(file)
 $(file) : $(LOCAL_PATH)/init.sapphire.rc | $(ACP)
 	$(transform-prebuilt-to-target)
 
-
 include $(CLEAR_VARS)
 LOCAL_SRC_FILES := sapphire-keypad.kcm
 include $(BUILD_KEY_CHAR_MAP)
 
-include $(CLEAR_VARS)
-LOCAL_MODULE_CLASS := ETC
-LOCAL_MODULE := vold.fstab
-LOCAL_SRC_FILES := $(LOCAL_MODULE)
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := wlan.ko
-LOCAL_MODULE_TAGS := user
-LOCAL_MODULE_CLASS := ETC
-LOCAL_MODULE_PATH := $(TARGET_OUT)/lib/modules
-LOCAL_SRC_FILES := $(LOCAL_MODULE)
-include $(BUILD_PREBUILT)
+include device/htc/dream-sapphire/AndroidBoardCommon.mk
 
 -include vendor/htc/sapphire/AndroidBoardVendor.mk
